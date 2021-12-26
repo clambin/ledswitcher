22edeccbc6aff3f9fa74f053daf1b617d5a092d0
clambin <christophe.lambin@gmail.com>
2021-12-26 12:49:27 +0100

    refactor: code reuse

--- controller/caller/caller.go
+++ controller/caller/caller.go
@@ -19,38 +19,28 @@ type HTTPCaller struct {
 }
 
 func (caller *HTTPCaller) SetLEDOn(targetURL string) (err error) {
-	var resp *http.Response
-	resp, err = caller.HTTPClient.Post(targetURL+"/led", "application/json", nil)
-
-	if err == nil {
-		if resp.StatusCode != http.StatusOK {
-			err = fmt.Errorf("%s", resp.Status)
-		}
-		_ = resp.Body.Close()
-	}
-	return
+	return caller.call(targetURL+"/led", http.MethodPost, nil)
 }
 
 func (caller *HTTPCaller) SetLEDOff(targetURL string) (err error) {
-	req, _ := http.NewRequest(http.MethodDelete, targetURL+"/led", nil)
-
-	var resp *http.Response
-	resp, err = caller.HTTPClient.Do(req)
-
-	if err == nil {
-		if resp.StatusCode != http.StatusOK {
-			err = fmt.Errorf("%s", resp.Status)
-		}
-		_ = resp.Body.Close()
-	}
-	return
+	return caller.call(targetURL+"/led", http.MethodDelete, nil)
 }
 
 func (caller *HTTPCaller) Register(leaderURL, clientURL string) (err error) {
 	body := fmt.Sprintf(`{ "url": "%s" }`, clientURL)
+	return caller.call(leaderURL+"/register", http.MethodPost, &body)
+}
+
+func (caller *HTTPCaller) call(endpoint, method string, body *string) (err error) {
+	var req *http.Request
+	if body != nil {
+		req, _ = http.NewRequest(method, endpoint, bytes.NewBufferString(*body))
+	} else {
+		req, _ = http.NewRequest(method, endpoint, nil)
+	}
 
 	var resp *http.Response
-	resp, err = caller.HTTPClient.Post(leaderURL+"/register", "application/json", bytes.NewBufferString(body))
+	resp, err = caller.HTTPClient.Do(req)
 
 	if err == nil {
 		if resp.StatusCode != http.StatusOK {
