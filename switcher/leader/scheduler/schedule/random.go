acf06c409267602479fb51cad1f0aae01c4017cf
clambin <christophe.lambin@gmail.com>
2023-08-16 13:42:49 +0200

    fix: rand.Seed no longer needed

--- switcher/leader/scheduler/schedule/random.go
+++ switcher/leader/scheduler/schedule/random.go
@@ -2,32 +2,24 @@ package schedule
 
 import (
 	"math/rand"
-	"time"
 )
 
 // RandomSchedule switches on a LED at random
 type RandomSchedule struct {
-	seeded bool
-	last   int
+	current int
 }
 
 var _ Schedule = &RandomSchedule{}
 
 // Next returns the next pattern
 func (s *RandomSchedule) Next(count int) []bool {
-	if !s.seeded {
-		rand.Seed(time.Now().UnixNano())
-		s.seeded = true
-		s.last = -1
-	}
-
 	var next int
 	for i := 0; i < 5; i++ {
-		next = rand.Intn(count)
-		if next != s.last {
+		next = 1 + rand.Intn(count)
+		if next != s.current {
 			break
 		}
 	}
-	s.last = next
-	return intToBits(1<<s.last, count)
+	s.current = next
+	return intToBits(1<<(s.current-1), count)
 }
