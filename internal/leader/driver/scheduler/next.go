5120927ea968337d281cb2f431b1ca44f374ccfc
Christophe Lambin <christophe.lambin@gmail.com>
2024-03-25 22:57:45 +0100

    refactor: complete refactoring to simplify code & handling of prometheus metrics

--- internal/leader/driver/scheduler/next.go
+++ internal/leader/driver/scheduler/next.go
@@ -7,7 +7,7 @@ type Action struct {
 }
 
 // Next determines the required actions for the next state
-func (s *Scheduler) Next() (actions []Action) {
+func (s *Scheduler) Next() []Action {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
@@ -19,26 +19,29 @@ func (s *Scheduler) Next() (actions []Action) {
 	}
 
 	// get the next state and, for each host that is not in the desired state, create an action
+	actions := make([]Action, 0, count)
 	for index, state := range s.Schedule.Next(count) {
 		host := hosts[index]
 		registeredHost := s.hosts[host]
-		if registeredHost.State != state {
-			registeredHost.State = state
-			s.hosts[host] = registeredHost
-			actions = append(actions, Action{
-				Host:  hosts[index],
-				State: state,
-			})
+		if registeredHost.State == state {
+			continue
 		}
+		registeredHost.State = state
+		s.hosts[host] = registeredHost
+		actions = append(actions, Action{
+			Host:  hosts[index],
+			State: state,
+		})
 	}
-	return
+	return actions
 }
 
-func (s *Scheduler) getActiveHosts() (hosts []string) {
+func (s *Scheduler) getActiveHosts() []string {
+	hosts := make([]string, 0, len(s.hostNames))
 	for _, host := range s.hostNames {
 		if s.hosts[host].IsAlive() {
 			hosts = append(hosts, host)
 		}
 	}
-	return
+	return hosts
 }
