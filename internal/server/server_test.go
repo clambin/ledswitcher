362904c8b4a1ef4ea4dd0043b6185bb73387c20c
Christophe Lambin <christophe.lambin@gmail.com>
2024-08-27 22:39:27 +0200

    refactor: full rewrite

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -14,6 +14,7 @@ import (
 func TestServer(t *testing.T) {
 	tests := []struct {
 		name       string
+		leading    bool
 		target     string
 		method     string
 		body       string
@@ -21,30 +22,49 @@ func TestServer(t *testing.T) {
 	}{
 		{
 			name:       "led on",
+			leading:    true,
 			target:     "/endpoint/led",
 			method:     http.MethodPost,
 			wantStatus: http.StatusCreated,
 		},
 		{
 			name:       "led off",
+			leading:    true,
 			target:     "/endpoint/led",
 			method:     http.MethodDelete,
 			wantStatus: http.StatusNoContent,
 		},
 		{
+			name:       "led - invalid method",
+			leading:    true,
+			target:     "/endpoint/led",
+			method:     http.MethodGet,
+			wantStatus: http.StatusMethodNotAllowed,
+		},
+		{
 			name:       "health",
+			leading:    true,
 			target:     "/health",
 			method:     http.MethodGet,
 			wantStatus: http.StatusServiceUnavailable,
 		},
 		{
 			name:       "leader stats",
+			leading:    true,
 			target:     "/leader/stats",
 			method:     http.MethodGet,
 			wantStatus: http.StatusOK,
 		},
 		{
+			name:       "leader stats - not leading",
+			leading:    false,
+			target:     "/leader/stats",
+			method:     http.MethodGet,
+			wantStatus: http.StatusServiceUnavailable,
+		},
+		{
 			name:       "register - invalid json",
+			leading:    true,
 			target:     "/leader/register",
 			method:     http.MethodPost,
 			body:       "invalid-json",
@@ -52,13 +72,23 @@ func TestServer(t *testing.T) {
 		},
 		{
 			name:       "register - invalid url",
+			leading:    true,
 			target:     "/leader/register",
 			method:     http.MethodPost,
 			body:       `{ "url": "" }`,
 			wantStatus: http.StatusBadRequest,
 		},
 		{
+			name:       "register - not leading",
+			leading:    false,
+			target:     "/leader/register",
+			method:     http.MethodPost,
+			body:       `{ "url": "http://localhost:1234" }`,
+			wantStatus: http.StatusServiceUnavailable,
+		},
+		{
 			name:       "register - valid url",
+			leading:    true,
 			target:     "/leader/register",
 			method:     http.MethodPost,
 			body:       `{ "url": "http://localhost:1234" }`,
@@ -66,16 +96,16 @@ func TestServer(t *testing.T) {
 		},
 	}
 
-	l := slog.Default()
-	var led fakeLEDSetter
-	r := registry.Registry{Logger: l}
-	r.Leading(true)
-	r.Register("http://localhost:8080")
-	s := New(&led, &fakeRegistrant{}, &r, l)
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
+			l := slog.Default()
+			var led fakeLEDSetter
+			r := registry.Registry{Logger: l}
+			r.Leading(tt.leading)
+			r.Register("http://localhost:8080")
+			s := New(&led, &fakeRegistrant{}, &r, l)
+
 			req, _ := http.NewRequest(tt.method, tt.target, bytes.NewBufferString(tt.body))
 			resp := httptest.NewRecorder()
 			s.ServeHTTP(resp, req)
