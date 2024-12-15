bad782c3fa645005f282c0c33ae30062b806ebe7
Christophe Lambin <christophe.lambin@gmail.com>
2024-12-16 00:32:42 +0100

    test: memory profile

--- ledswitcher.go
+++ ledswitcher.go
@@ -4,14 +4,21 @@ import (
 	"context"
 	"fmt"
 	"github.com/clambin/ledswitcher/internal/cmd"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	_ "net/http/pprof"
 )
 
 var version = "change-me"
 
 func main() {
+	go func() {
+		_ = http.ListenAndServe(":6060", nil)
+	}()
+
 	ctx, done := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer done()
 	if err := cmd.Main(ctx, version); err != nil {
