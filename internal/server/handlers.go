96af1a2db5cac8ccf782521642af073d73fe9dbe
Christophe Lambin <christophe.lambin@gmail.com>
2025-06-05 23:44:02 +0200

    feat: event driven approach, using redis as pub/sub

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -6,6 +6,7 @@ func HealthHandler(s *Server) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if err := s.Endpoint.eventHandler.Client.Ping(r.Context()).Err(); err != nil {
 			http.Error(w, "redis: "+err.Error(), http.StatusServiceUnavailable)
+			return
 		}
 		w.WriteHeader(http.StatusOK)
 	})
