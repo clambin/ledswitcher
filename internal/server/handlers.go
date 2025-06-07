453ade1ca4b0710980b5a6936324f09d17e5e1a6
Christophe Lambin <christophe.lambin@gmail.com>
2025-06-07 12:50:47 +0200

    feat: log error if health check fails

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -5,6 +5,7 @@ import "net/http"
 func HealthHandler(s *Server) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if err := s.Endpoint.ping(r.Context()); err != nil {
+			s.Endpoint.logger.Warn("health check failed", "err", err)
 			http.Error(w, "redis: "+err.Error(), http.StatusServiceUnavailable)
 			return
 		}
